b49e1df7281d98f723491705e9cf77f5696540e4
orfeas-menis <34897761+orfeas-menis@users.noreply.github.com>
2019-09-09 02:44:12 +0300

    Create Mural.cpp

---
+++ Practice Round/Mural.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    long long int N,sum,count,max,limit,temp;
+    int T,i;
+    cin >> T;
+    for (i=1; i<=T; i++){
+        sum = 0;
+        count = 0;
+        max = 0;
+        cin >> N;
+        if(N%2 == 0){
+            limit = N/2;
+        }
+        else{
+            limit = N/2 + 1;
+        }
+        int Arr[N],j;
+        cin >> temp;
+
+        for (j=0; j<N; j++){
+            Arr[j] = temp % 10;
+            temp = temp / 10;
+            if (count<limit){
+                count++;
+                sum = sum + Arr[j];
+            }
+            else{
+                sum = sum + Arr[j] - Arr[j-limit];
+            }
+            if (max < sum){
+                max = sum;
+            }
+        }
+        
+        cout << "Case #" + to_string(i) + ": " + to_string(max) << endl;
+        cout.flush();
+    }
+    return 0;
+}
